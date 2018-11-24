6b81e5fca3ad244db269f3102769fe4138f90413
santhosh <singumahanthisanthosh@gmail.com>
2018-11-24 14:31:26 +0530

    express app setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/',(req,res)=>{
+    res.send('welcome to wordlink application')
+})
+app.listen(port,()=>{
+    console.log(`listening to server on ${port} port`);
+    
+})
+module.exports = app;
