55b59d4046c0d2ed58910c8499c14bdd6b853062
santhosh <singumahanthisanthosh@gmail.com>
2018-11-25 17:43:00 +0530

    oxford dictionary api used in app.js file

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const port = process.env.PORT || 8080;
 const app = express();
+const Dictionary = require('oxford-dictionary-api');
+const app_id = process.env.appId;
+const app_key = process.env.appKey ;
+console.log(app_id);
+console.log(app_key);
+const dict = new Dictionary(app_id,app_key);
+
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
@@ -13,7 +22,12 @@ app.get('/',(req,res)=>{
         'msg' :'welcome to wordlink application!'
     });
 });
-const wordRoutes = require('./routes/word');
+dict.find("rainbow",function(error,data){
+    if(error) return console.log(error);
+    console.log(data);
+  });
+  
+const wordRoutes = require('./routes/words');
 app.use('/word',wordRoutes);
 
 app.listen(port,()=>{
--- routes/words.js
+++ routes/words.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router  = express.Router();
 const wordsController = require('../controllers/words');
-router.get('/',wordsController.words_get_all);
+// router.get('/',wordsController.words_get_all);
 module.exports = router;
